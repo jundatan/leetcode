fc745c366d2a252370b35717f425dcbe4cb7e1b9
Tan Jun Da <e0726893@u.nus.edu>
2024-05-16 23:47:36 +0800

    Add Maximize Sum Of Array After K Negations
    Difficulty: Easy

---
+++ Easy/MaximizeSumOfArrayAfterKNegations.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def largestSumAfterKNegations(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        for i in range(k):
+            nums.sort()
+            nums[0] = -1 * nums[0]
+        return sum(nums)
